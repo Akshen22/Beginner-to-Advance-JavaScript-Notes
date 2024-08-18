109e357743534dccdd9e983a445c11e05283ac13
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-18 23:58:42 +0530

    Update 02.2) Variables.js

--- 02.2) Variables.js
+++ 02.2) Variables.js
@@ -64,4 +64,14 @@ In the example given above, we are storing the result of an expression 23 + 45 a
 In the next line, we are logging the value of sum to the console. We see that the console displays 68 and not the expression 24 + 45 itself.
 This is because the expression is first evaluated, then the result of this evaluation, 68, is stored as a value for the variable sum.
 
-14) 
+14) Challenge:
+We have provided below the results of the scores of Harry, Ron, and Sam in a test. 
+Calculate and store the average marks scored by all three in a variable. Also, log it to the console.
+
+const markScoredByHarry = 7;
+const markScoredByRon = 7;
+const markScoredBySam = 11;
+const avg = (markScoredByHarry + markScoredByRon + markScoredBySam) / 3;
+console.log(avg);
+
+15) 
