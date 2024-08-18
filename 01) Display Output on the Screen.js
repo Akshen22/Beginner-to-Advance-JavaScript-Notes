3c7f6b60cab6b1ba67a83ae925c0ef264bb9bc66
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-18 22:35:47 +0530

    Create 01) Display Output on the Screen.js

---
+++ 01) Display Output on the Screen.js
@@ -0,0 +1,17 @@
+1) comments: /* */
+
+2) Showing Message in console:
+In JavaScript, content just doesn't appear on the screen by itself. 
+We need to use "console.log()" to make it visible on a special screen called console.
+
+Example:
+a) console.log("I love ice cream");
+b) console.log(20-10)
+
+3) Console - Logging multiple values:
+If you want to put multiple messages on the screen, you can put them in the same console.log(). 
+Separate the different values using commas.
+
+Example: 
+a) console.log(1+8, 10+20) /* Ans) 9,30*/
+b) console.log("Good morning!", "Have a good day!") 
