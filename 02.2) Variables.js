ef2f7a593b8934d59912815dddf3a3d18951ba87
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-18 23:56:01 +0530

    Update 02.2) Variables.js

--- 02.2) Variables.js
+++ 02.2) Variables.js
@@ -53,6 +53,15 @@ i) Note that, operators that require two operands are called binary operators. +
 ii) Operators that require only one operand are called unary operators. typeof is a unary operator.
 iii) The operators +, -, *, and / are called arithmetic operators and are used to perform arithmetic operations on numbers.
 
+13) Storing Result of Expressions:
+When we add an expression as a value to a variable, it first gets evaluated and then the result is stored in the variable.
 
+Consider the following example:
+const sum = 23 + 45;
+console.log(sum);
 
+In the example given above, we are storing the result of an expression 23 + 45 as a value to the variable sum.
+In the next line, we are logging the value of sum to the console. We see that the console displays 68 and not the expression 24 + 45 itself.
+This is because the expression is first evaluated, then the result of this evaluation, 68, is stored as a value for the variable sum.
 
+14) 
