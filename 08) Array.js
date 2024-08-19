a48ae86970c1836899774d05f6703dbecc294ba5
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 14:56:00 +0530

    Update 08) Array.js

--- 08) Array.js
+++ 08) Array.js
@@ -1,10 +1,12 @@
 1) Create Array:
-An array is used to store multiple values together.
+
+a) An array is used to store multiple values together.
 An array is built by a pair of square brackets, with values placed inside it separated by commas.
-  
 Let us take an example of a list of fruits:
 const fruits = ["Apple", "Orange", "Banana"];
-
 console.log(fruits);
 
-2) 
+b) Arrays in JavaScript are special kind of objects. Similar to an object, it can hold a mix of different data types.
+Consider the following example:
+const mixedArray = [1, True, "Hi", null, undefined];
+console.log(mixedArray);
