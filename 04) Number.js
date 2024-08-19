3b5bc5ab695fffe38c46816251507911c132aa59
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 08:41:04 +0530

    Update 04) Number.js

--- 04) Number.js
+++ 04) Number.js
@@ -1,9 +1,14 @@
 1) Store Number:
 a) Positive number:
+const positiveNumber = 1234;
+console.log(positiveNumber);
 
 b) Negative number:
+const negativeNumber = -1234;
+console.log(negativeNumber);
 
 c) Fractional number:
-
+const fractionalNumber = 12.34;
+console.log(fractionalNumber);
 
 2) 
