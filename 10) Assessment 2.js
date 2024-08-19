b11b0e5c0e19dfd1cc718076717057b69563135e
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 20:22:00 +0530

    Update 10) Assessment 2.js

--- 10) Assessment 2.js
+++ 10) Assessment 2.js
@@ -29,3 +29,29 @@ const totalWorldCupWins = countryDetails.worldCupWinningYears.length;
 console.log(`${countryDetails.name} has won ${totalWorldCupWins} FIFA World Cups.`)
 
 2) Print Company Details:
+If the details of an office are provided in the below format:
+
+Key	Value
+name	Tata Sons
+address	BOMBAY HOUSE 24 HOMI MODY STREET
+city	Mumbai
+state	Maharashtra
+pinOrZipCode	400001
+country	India
+Write a code that takes in these office details to print a statement in the format as shown below:
+
+[Company Name] is from [City Name], [State Name] in [Country Name]. Their pincode/zipcode is [Pin or Zip code].
+
+Solution:
+const officeDetails = {
+	name: "Tata Sons",
+	address: "BOMBAY HOUSE 24 HOMI MODY STREET",
+	city: "Mumbai",
+	state: "Maharashtra",
+	pinOrZipCode: "400001",
+	country: "India"
+};
+
+console.log(`${officeDetails.name} is from ${officeDetails.city}, ${officeDetails.state} in ${officeDetails.country}. Their pincode/zipcode is ${officeDetails.pinOrZipCode}.`)
+
+3) Even Roll Call:
