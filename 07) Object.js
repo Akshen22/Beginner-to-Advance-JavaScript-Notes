fb06439f1aeb37a9be3e5a961ba731917be82f15
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 10:48:33 +0530

    Create 07) Object.js

---
+++ 07) Object.js
@@ -0,0 +1,8 @@
+1) Create Object:
+We can use the Object type to store related compound values together, like all the details of a car. 
+For example, the fuelType of a car can be stored within a car object.
+const car = {
+  fuelType: "diesel"
+};
+
+console.log(car);
