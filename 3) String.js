d0a9519505832eed3704eec8d27c760e0388694d
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 07:44:16 +0530

    Create 3) String.js

---
+++ 3) String.js
@@ -0,0 +1,42 @@
+1) quotes:
+a) double quotes:
+const fullName = "Sam Smith";
+console.log(fullName);
+
+const book = "The Newest Dragon Stories";
+console.log(book);
+
+const someRandomSentence = "You can be a great programmer!";
+console.log(someRandomSentence);
+
+b) single quote:
+const fullName = 'Sam Smith';
+console.log(fullName);
+
+const book = 'The Newest Dragon Stories';
+console.log(book);
+
+const someRandomSentence = 'You can be a great programmer!';
+console.log(someRandomSentence);
+
+//We can see that using single quotes or double quotes for creating strings makes no difference. 
+But moving forward, to maintain consistency in the Academy, we will be using double quotes when creating strings.
+
+2) Joining Strings:
+a) We can join strings using the + operator. 
+And we can store the joined result into a new variable of our choice.
+
+const firstName = "Sam";
+const lastName = "Smith";
+const fullName = firstName + lastName;
+
+console.log(fullName);
+
+b) Insert a space between the two strings:
+const firstName = "Sam";
+const lastName = "Smith";
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+3) 
