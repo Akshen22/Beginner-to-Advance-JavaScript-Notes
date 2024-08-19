3a39e9ddccad33957f689236ede7c8cba1cf916f
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 11:46:25 +0530

    Update 07) Object.js

--- 07) Object.js
+++ 07) Object.js
@@ -30,7 +30,18 @@ console.log(result);
 
 In the example given above, the property car.seatingCapacity is assigned the value of noOfSeats, which is 5.
 
-4) Challenge:
+4) Get Object Values - Dot Notation:
+We can read the values of properties of an object using the dot notation.
+
+Example:
+const person = {
+  name: "Oliver",
+  hobby: "Writing"
+};
+
+console.log(person.name);
+
+5) Challenge:
 Create a variable library. Value of library is an object and the properties of that object are defined below:
 
 Key: Value
@@ -38,7 +49,7 @@ fiction:	"Harry Potter"
 autobiography:	"The diary of a young girl"
 history:	"The Discovery of India"
 
-Log the value of library to the console.
+Later, log the value of fiction and autobiography to the console using dot notation.
 
 Solution:
 const library = {
@@ -47,8 +58,7 @@ const library = {
   history: "The Discovery of India"
 }
 
-console.log(library);
+console.log(library.fiction, library.autobiography);
 
-5) 
-
+6) 
 
