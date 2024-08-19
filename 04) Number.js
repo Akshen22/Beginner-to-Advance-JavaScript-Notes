d6f22615830613b758133dbb5ee81d2ae977049d
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 08:54:32 +0530

    Update 04) Number.js

--- 04) Number.js
+++ 04) Number.js
@@ -45,3 +45,22 @@ firstNumber++;
 secondNumber--;
 
 console.log(firstNumber, secondNumber);
+
+d) Challenge:
+Create a variable firstNumber which stores the value of 3. 
+Create another variable secondNumber which stores the value 5.
+Increase the value of firstNumber by one. Similarly, decrease the value of secondNumber by one.
+Later check if firstNumber and secondNumber are equal or not, and log the result to the console.
+Output should be a Boolean.
+
+Solution:
+let firstNumber = 3;
+let secondNumber = 5;
+
+firstNumber++;
+secondNumber--;
+
+let result = (firstNumber===secondNumber);
+console.log(result);
+
+4) 
