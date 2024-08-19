3b3269c09f3942e20956ef44a6824e584e1d6d18
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 08:43:17 +0530

    Update 04) Number.js

--- 04) Number.js
+++ 04) Number.js
@@ -11,4 +11,15 @@ c) Fractional number:
 const fractionalNumber = 12.34;
 console.log(fractionalNumber);
 
+d) Challenge:
+Create a variable named phoneNumber and store the number 1234567890 as the value. 
+Create another variable named pinCode and store the value 2255 in it.
+At the end, put the values of both variables in the console.
+
+Solution:
+const phoneNumber = 1234567890;
+const pinCode = 2255;
+
+console.log(phoneNumber, pinCode);
+
 2) 
