ef2fa79e54a5972f4729e92aa10179f64f02bf7f
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 08:21:07 +0530

    Update 3) String.js

--- 3) String.js
+++ 3) String.js
@@ -57,4 +57,20 @@ console.log(`Adding ${firstNum} and ${secondNum} gives ${result}`);
 //Template strings are enclosed within backticks ` instead of single or double quotes. 
 // We don't need to use '+' operator to concatenate strings.
 
+c) Challenge:
+Create a variable named firstName and store the string Sam as the value. 
+Create another variable named lastName and store the string Smith as the value. 
+Join both firstName and lastName and assign it to a new variable named fullName.
+Finally using the variables firstName, lastName and fullName, print the below two sentences to the console.
+Your first name is [First Name] and last name is [Last Name].
+Good Morning [Full Name]! Have a great day.
+
+Solution:
+const firstName = "Sam";
+const lastName = "Smith";
+const fullName = firstName+ " " +lastName;
+
+console.log(`Your first name is ${firstName} and last name is ${lastName}.`);
+console.log(`Good Morning ${fullName}! Have a great day.`)
+
 4) 
