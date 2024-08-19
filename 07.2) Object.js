150886f51d4ba8f33a0751d46fc3de4d58aa4a49
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 13:42:22 +0530

    Update 07.2) Object.js

--- 07.2) Object.js
+++ 07.2) Object.js
@@ -1,1 +1,40 @@
-12) Get Object Keys - Object.keys():
+12) Get Object Keys - Object.keys(): helps to get keys!
+
+const person = {
+  fullName: "Adam Smith",
+  age: 37,
+  isAdmin: true
+};
+
+const result = Object.keys(person);
+console.log(result);
+
+13) Get Object Keys - Object.values(): helps to get values!
+
+const person = {
+  fullName: "Adam Smith",
+  age: 37,
+  isAdmin: true
+};
+
+const result = Object.values(person);
+console.log(result);
+
+14) Get Object Keys and Values - Object.entries(): helps to get key-value pairs!
+
+const person = {
+  fullName: "Adam Smith",
+  age: 37,
+  isAdmin: true
+};
+
+const result = Object.entries(person);
+console.log(result);
+
+15) 
+
+
+
+
+
+
