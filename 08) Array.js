d1e97cb89584dcc8e095156d1f949987abb611b6
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-19 14:51:22 +0530

    Create 08) Array.js

---
+++ 08) Array.js
@@ -0,0 +1,10 @@
+1) Create Array:
+An array is used to store multiple values together.
+An array is built by a pair of square brackets, with values placed inside it separated by commas.
+  
+Let us take an example of a list of fruits:
+const fruits = ["Apple", "Orange", "Banana"];
+
+console.log(fruits);
+
+2) 
