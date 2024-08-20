21c875b15ea83d91ca3316c2d2ef74c47c25f07c
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-20 08:59:49 +0530

    Update 11.2) Conditionals.js

--- 11.2) Conditionals.js
+++ 11.2) Conditionals.js
@@ -32,4 +32,26 @@ v) The value of the switch expression is then compared with the value of each ca
 vi) The code block of the first case whose value is equal to the value of the switch expression is executed.
 vii) If the value of the switch expression does not match with any of the cases, then the default code block is executed.
 
-7) 
+7) switch statement fall through method (multiple cases at once!)
+
+const passedRollNumbers = [8746, 5649, 3268, 7901];
+const otherRollNumbers = [4387, 1752, 6210, 9834];
+const rollNumberToCheck = 7901;
+
+switch(rollNumberToCheck) {
+  case 8746: case 5649: case 3268: case 7901:
+    console.log("Congratulations, you have passed the test!");
+    break;
+
+  case 4387: case 1752: case 6210: case 9834:
+    console.log("Unfortunately, you did not pass the test. Better luck next time!");
+    break;
+
+  default:
+    console.log("Provided roll number is invalid");
+};
+
+Here for the roll numbers 8746, 5649, 3268, 7901 the code to be executed is the same: console.log("Congratulations, you have passed the test!");. 
+So we group them together and write the code to be executed once. We do the same for the other set as well.
+
+8) 
