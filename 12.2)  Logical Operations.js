a24a53fb917d9874e6a54262430dfde35ad3e78b
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-20 20:02:54 +0530

    Update 12.2)  Logical Operations.js

--- 12.2)  Logical Operations.js
+++ 12.2)  Logical Operations.js
@@ -116,3 +116,23 @@ const user2 = {
 };
 console.log(`${user2.name} is a ${user2.role ?? defaultRole} user.`);
 
+c) Challenge:
+In the example given below, user2 does not have the property noOfAttempts, which causes an unwanted output when totalNoOfAttempts - user2.noOfAttempts is executed.
+Use the ?? operator to use the default value of the noOfAttempts as 0.
+
+Solution:
+const defaultRole = 0;
+
+const totalNoOfAttempts = 5;
+
+const user1 = {
+  name: "Sam Smith",
+  noOfAttempts: 3,
+};
+
+const user2 = {
+  name: "Oliver Smith",
+};
+
+console.log(`${user1.name} has ${totalNoOfAttempts - (user1.noOfAttempts ?? defaultRole)} attempts left.`);
+console.log(`${user2.name} has ${totalNoOfAttempts - (user2.noOfAttempts ?? defaultRole)} attempts left.`);
