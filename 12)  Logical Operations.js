74094206d98203cab3144d27046738524376c4b2
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-20 17:49:04 +0530

    Update 12)  Logical Operations.js

--- 12)  Logical Operations.js
+++ 12)  Logical Operations.js
@@ -90,4 +90,49 @@ true	              false	                  false
 false	              true	                  false
 false	              false	                  false
 
-3) 
+3) AND Challenge:
+You are working on a user management system for an online platform.
+You have a user object representing a user in the system. The user object contains properties such as name, login status, role, plan, and fullAdminPackage. 
+Your task is to write code that checks the user's login status, role, plan, and fullAdminPackage to determine the access level to the admin panel.
+
+Check if the user is logged in using the isLogged property. If the user is logged in, log to the console: "User has logged in."
+Based on the user's role, plan, and fullAdminPackage, log the appropriate message regarding access to the admin panel:
+If the user's role is Admin and they are on the Premium plan, log: "Displaying the Admin Panel with all the admin features."
+If the user's role is Admin, they are on the Plus plan, and have the fullAdminPackage set to true, log: "Displaying the Admin Panel with all the admin features."
+If the user's role is Admin, they are on the Plus plan, but do not have the fullAdminPackage set to true, log: "Displaying the Admin Panel with only the Plus plan admin features."
+If the user's role is Admin and they are on the Free plan, log: "Displaying the Admin Panel with limited admin features."
+If the user's role is Standard, log: "Access denied."
+Use the provided user object for this challenge.
+
+Solutions:
+const user = {
+  name: "Sam",
+  isLogged: true,
+  role: "Admin",
+  plan: "Premium",
+  fullAdminPackage: true,
+};
+
+if (user.isLogged === true) {
+  console.log(`User has logged in.`)
+};
+
+if (user.role === "Admin") {
+  if (user.plan === "Premium") {
+  console.log(`Displaying the Admin Panel with all the admin features.`);
+} else if (user.plan === "Plus" && user.fullAdminPackage === true) {
+  console.log(`Displaying the Admin Panel with all the admin features.`);
+} else if (user.plan === "Plus" && user.fullAdminPackage === false) {
+  console.log("Displaying the Admin Panel with only the plus with limited admin features.");
+} else if (user.plan === "Free") {
+  console.log(`Displaying the Admin Panel with limited admin features.`);
+} else {
+  console.log("Access denied");
+}
+}
+
+else {
+  console.log("Please log in to access the admin panel.")
+}
+
+4) 
