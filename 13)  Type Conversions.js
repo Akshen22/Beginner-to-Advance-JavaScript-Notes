e9377ae75d88c8b4523be8b8419a0724d035a1b3
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-20 20:09:41 +0530

    Update 13)  Type Conversions.js

--- 13)  Type Conversions.js
+++ 13)  Type Conversions.js
@@ -1,1 +1,5 @@
+1) Converting to String:
+Sometimes we need to explicitly convert a value to string. 
+In such cases, we can use either String() (most preferred) or toString() to convert a non-string value to a string.
+
 
