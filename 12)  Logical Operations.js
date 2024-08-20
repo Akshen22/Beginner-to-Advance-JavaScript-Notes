37855266e03dc7eff644ba89a10ce224895b03e9
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-20 17:09:37 +0530

    Update 12)  Logical Operations.js

--- 12)  Logical Operations.js
+++ 12)  Logical Operations.js
@@ -35,4 +35,23 @@ if (student.grade === null || student.grade === undefined) {
 and therefore we get the output "Student grade must be provided".
 We would get the same output if the value of the student. grade variable was null.*/
 
+Example 3:
+In the code below, if the user is an admin or a moderator, log to the console: [User name] has special privileges.
+If not, log to the console: [User name] has standard privileges.
+
+Solution:
+const user = {
+  name: "Sam",
+  isAdmin: false,
+  isModerator: false
+};
+
+if (user.isAdmin === true || user.isModerator === true) {
+  console.log(`${user.name} has special privileges.`);
+}
+
+else {
+  console.log(`${user.name} has standard privileges.`)
+}
+
 2) 
