f8746b291f87b7343605af3b23a5fe6709d9a6f2
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-22 00:13:26 +0530

    Update 14.2) Functions.js

--- 14.2) Functions.js
+++ 14.2) Functions.js
@@ -1,1 +1,9 @@
+1) Parameters & Arguments:
+const displayValidationError = (invalidFieldName, additionalMessage) => { // parameters
+  const result = `The user ${invalidFieldName} is not valid. ${additionalMessage}`;
+  console.log(result);
+}
 
+displayValidationError("name", "Please provide a valid name."); // arguments
+
+2) 
