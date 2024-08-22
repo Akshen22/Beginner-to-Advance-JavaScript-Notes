8e4d196dfdebc5db37b21220a5cae3c0abbd58f7
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-23 00:00:01 +0530

    Update 14.3) Functions.js

--- 14.3) Functions.js
+++ 14.3) Functions.js
@@ -1,5 +1,6 @@
 9) return Statement for Terminating Functions:
-After the return statement in a function is executed, further execution of the function is terminated. Consider the following example:
+After the return statement in a function is executed, further execution of the function is terminated. 
+Consider the following example:
 const calculateSum = (number1, number2) => {
   console.log("Calculating...");
 
