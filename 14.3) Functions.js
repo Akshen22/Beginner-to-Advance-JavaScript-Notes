146da562e2781a7a88686d86889d131d3d06b223
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-23 00:13:44 +0530

    Update 14.3) Functions.js

--- 14.3) Functions.js
+++ 14.3) Functions.js
@@ -18,5 +18,34 @@ When the calculateSum function is invoked, first we get the output "Calculating.
 Then the sum number1 + number2 is returned.
 After the return statement, the function execution is stopped. The statement console.log("Calculation complete") is not executed and hence we don't see it in the output.
 
-b) 
+b) const student1 = {
+  name: "Sam Smith",
+  nationality: "India",
+};
 
+const student2 = {
+  name: "Oliver Smith",
+  nationality: "USA",
+};
+
+const courseFee = 5000;
+
+const getStudentFee = student => {
+  if (student.nationality !== "India")
+    return courseFee;
+
+  const discountedFee = courseFee - courseFee * 0.1;
+  return discountedFee;
+};
+
+console.log(getStudentFee(student1));
+
+console.log(getStudentFee(student2));
+
+In the example given above, in the function getStudentFee:
+
+If the value of the nationality property of the parameter student is not "India", the value of courseFee is returned.
+If the value of student.nationality is "India", a 10% discounted course fee is calculated, stored in discountedFee, and the value of discountedFee is returned.
+We can rewrite the getStudentFee function in the example given above, to make use of the fact that the return statement terminates further execution of a function.
+
+c) 
