a2520b719c47e27c14ec2dd190d4615436c5500f
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-22 23:59:29 +0530

    Update 14.3) Functions.js

--- 14.3) Functions.js
+++ 14.3) Functions.js
@@ -1,1 +1,2 @@
 9) return Statement for Terminating Functions:
+After the return statement in a function is executed, further execution of the function is terminated. Consider the following example:
