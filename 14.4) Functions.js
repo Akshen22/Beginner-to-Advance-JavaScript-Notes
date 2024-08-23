71dd1ed8e3ace921ce326ec8e1d591ab3f46fa7e
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-23 15:21:07 +0530

    Update 14.4) Functions.js

--- 14.4) Functions.js
+++ 14.4) Functions.js
@@ -13,23 +13,35 @@ admin.displayGreeting();
 In the example given above, we have defined an object admin. Then a function is assigned to the property displayGreeting of the admin object. 
 displayGreeting is now a method of the admin object.
 
-12) We can also assign a pre-defined function to an object property.
+12) Shorthand for Method:
+We can define methods directly inside an object's definition.
 
-const admin = {
+a) const admin = {
   name: "Adam Smith",
   age: 45,
+  displayGreeting: () => console.log("Hello everyone"),
 }
-
-const displayHelloWorld = () => console.log("Hello World");
-
-admin.displayGreeting = displayHelloWorld;
 
 admin.displayGreeting();
 
-In the example given above, we first defined the displayHelloWorld function. 
-Then we assigned it to the displayGreeting property of admin.
+Other ways:
+b) const admin = {
+  name: "Adam Smith",
+  age: 45,
+  displayGreeting: function () {
+    console.log("Hello everyone");
+  }
+}
 
-This way of defining an object method is not preferred. 
-We'll take a look at the preferred way of defining object methods in the next chapter.
+admin.displayGreeting();
+
+c) const name = "Adam Smith";
+const age = 45;
+const displayGreeting = () => console.log("Hello everyone");
+
+const admin = { name, age, displayGreeting };
+
+admin.displayGreeting();
+console.log(`I am ${admin.name}`);
 
 13) 
