b4d8bd17d762e92f019d93ea2bc17ed07a33f044
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-26 13:37:36 +0530

    Update 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js

--- 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js
+++ 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js
@@ -73,4 +73,50 @@ const calculateShippingCostIndia = (weight, destination) => {
   return `Shipping cost is ₹${totalCost}`;
 }
 
-7) 
+7) Income Tax Calculator:
+
+Update the function calculateTax to calculate the income tax for a given salary and age group in a fictional country. 
+The function should consider the salary amount and the age of the individual to determine the tax rate.
+
+The function parameters are,
+
+salary (number): It represents the annual salary of the individual in the local currency.
+age (number): It represents the age of the individual.
+The income tax is calculated based on the following criteria,
+
+For individuals aged 60 years or below,
+
+No tax for salaries up to ₹2,50,000.
+5% tax for salaries above ₹2,50,000 and up to ₹5,00,000.
+10% tax for salaries above ₹5,00,000.
+For individuals aged above 60 years,
+
+No tax for salaries up to ₹3,00,000.
+5% tax for salaries above ₹3,00,000 and up to ₹5,00,000.
+10% tax for salaries above ₹5,00,000.
+The function should return a string indicating the tax amount based on the salary and age, for example, "Tax amount is ₹X", where X is the calculated tax.
+
+Solution:
+const calculateIncomeTax = (salary, age) => {
+  // write your code here
+  let taxRate = 0;
+  let taxAmount = 0;
+
+  if (age <= 60) {
+    if (salary > 250000 && salary <= 500000) {
+      taxRate = 0.05;
+    } else if (salary > 500000) {
+      taxRate = 0.10;
+    }
+  } else {
+    if (salary > 300000 && salary <= 500000) {
+      taxRate = 0.05;
+    } else if (salary > 500000) {
+      taxRate = 0.10;
+    }
+  }
+
+  taxAmount = salary * taxRate;
+
+  return `Tax amount is ₹${taxAmount}`;
+}
