cd76f59edf9ef5ee8a1e992504653d74f6f6c1b1
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-26 13:08:46 +0530

    Update 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js

--- 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js
+++ 15.2) Discount Calculator,Shipping Cost Calculator & Income Tax Calculator  Assessment.js
@@ -1,4 +1,4 @@
-6) Discount Calculator:
+5) Discount Calculator:
 
 Update the function getDiscountedPrice to calculate the discounted price of an item based on the original price, membership status, and coupon availability. 
 The function should return the discounted price.
@@ -40,5 +40,22 @@ const getDiscountedPrice = (originalPrice, isMember, hasCoupon) => {
   return discountedPrice;
 }
 
-7) Shipping Cost Calculator:
+6) Shipping Cost Calculator:
 
+Update the function calculateShippingCostIndia to calculate the shipping cost for packages sent to destinations within India. 
+The function should consider the weight of the package and the destination state to determine the shipping cost.
+
+The function parameters are,
+
+weight (number): It represents the weight of the package in kilograms.
+destination (string): It represents the destination state within India.
+The shipping cost is calculated based on the following criteria,
+
+For all destinations in India, the base shipping cost is ₹50.
+Additionally, there is an extra charge of ₹10 per kilogram for packages weighing more than 5 kilograms.
+The function should return a string indicating the shipping cost based on the weight and destination,
+
+If the weight of the package is 5 kilograms or less, the function should return "Shipping cost is ₹X", where X is the total shipping cost.
+If the weight of the package exceeds 5 kilograms, the function should return "Shipping cost is ₹Y", where Y is the total shipping cost including the additional charge for weight above 5 kilograms.
+
+Solution:
