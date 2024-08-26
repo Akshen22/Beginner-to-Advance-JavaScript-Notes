94b58c4448150115853d5877b7ab363aa47ccb30
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-26 16:38:54 +0530

    Update 16) Array Method 2.js

--- 16) Array Method 2.js
+++ 16) Array Method 2.js
@@ -1,1 +1,22 @@
 1) Execute Function for Each Element:
+
+a) forEach():
+The forEach() method is like a friendly guide that helps us visit each element in an array one by one.
+It allows us to perform a specific action on every element without changing the original array.
+
+Let us look at an example:
+const fruits = ["apple", "banana", "mango", "orange"];
+
+const displayFruit = fruit => {
+  console.log(`${fruit} is a fruit.`);
+};
+
+const displayFruits = fruit => {
+  console.log(`${fruit} is a tasty fruit.`);
+};
+
+fruits.forEach(fruit => displayFruit(fruit));
+
+fruits.forEach(fruit => displayFruits(fruit));
+
+b) 
