c2ed14b5d50de052b591f894d3ee2a357b936114
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-26 17:03:19 +0530

    Update 16) Array Method 2.js

--- 16) Array Method 2.js
+++ 16) Array Method 2.js
@@ -22,4 +22,25 @@ const fruits = ["apple", "banana", "mango", "orange"];
 fruits.forEach(fruit => console.log(`${fruit} is a fruit.`));
 fruits.forEach(fruit => console.log(`${fruit} is a tasty fruit.`));
 
-2) 
+c) Challenge:
+In the code below, we have an array of objects. Each object holds details of a product.
+Log the details of each product in the following manner: Product: [Product Name], Price: $[Product Price]
+Calculate the total price of all products and log it to the console.
+
+Solution:
+const products = [
+  { id: 1, name: "Laptop", price: 999 },
+  { id: 2, name: "Phone", price: 499 },
+  { id: 3, name: "Tablet", price: 299 }
+];
+
+let totalPrice = 0;
+
+products.forEach(products => {
+  console.log(`Product: ${products.name}, Price: $${products.price}`);
+  totalPrice += products.price;
+});
+
+console.log(`Total Price: $${totalPrice}`);
+
+2) Map a Function and Get a New Array:
