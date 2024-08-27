83298e0eaeaffc3cdaf2faa4df41ced1e9d91877
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-27 17:15:47 +0530

    Update 17.2) Loops (break & continue).js

--- 17.2) Loops (break & continue).js
+++ 17.2) Loops (break & continue).js
@@ -27,4 +27,38 @@ for (let index = 0; index < 6; index++) {
   console.log(fruits[index]);
 }
 
-8) continue statement:
+8) continue statement: used to skip an iteration of a loop by immediately ending the current iteration of the loop, & moving to the next iteration.
+
+Example 1:
+let iteration = 1;
+while (iteration <= 10) {
+  iteration++;
+  if (iteration % 2 === 0) {
+    continue;
+  }
+  console.log(iteration);
+}
+
+/*During each iteration, we check the value of iteration to see if it is even, using the condition - iteration % 2 === 0. 
+If the condition is true, the continue statement is executed, which ends the current iteration and starts the next iteration of the loop.*/
+
+Example 2:
+for (let iteration = 1; iteration <= 10; iteration++) {
+  if (iteration % 2 === 0) {
+    continue;
+  }
+  console.log(iteration);
+}
+
+//used to display the odd numbers from 1 to 10 using a for loop.
+
+Challenge:
+The for loop given below prints the numbers from 1 to 30. Use the continue statement in the for loop to print only the numbers that are divisible by 3.
+
+Solution:
+for (let iteration = 0; iteration <= 30; iteration++) {
+  if(iteration % 3 != 0) {
+    continue;
+  }
+  console.log(iteration);
+}
