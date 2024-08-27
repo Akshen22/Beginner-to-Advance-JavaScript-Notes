df1f8d997a0978d03197e5e095bb48ca0edb9bb6
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-27 14:43:26 +0530

    Update 17) Loops.js

--- 17) Loops.js
+++ 17) Loops.js
@@ -1,1 +1,13 @@
 1) for loop:
+for (let abc = 1; abc < 6; abc++) {
+  console.log(abc);
+}
+
+Steps:
+Step 1: Before the loop starts, `let abc = 1` is run, setting `abc` to 1. This only happens once before the loop begins.
+Step 2: The loop checks if `abc < 6`. If true, the loop runs. Since `abc` starts at 1, the condition is true, so the loop runs.
+Step 3: After running the loop's code once (one iteration), `abc++` increases its value by 1.
+Step 4: The loop then checks the condition again (`abc < 6`). If true, it repeats the process. If false, the loop stops.
+The loop keeps running until `abc` reaches 6. Then, the condition is false, and the loop stops.
+
+ 2) 
