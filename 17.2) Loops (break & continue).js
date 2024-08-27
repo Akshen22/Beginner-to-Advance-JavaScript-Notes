5619011a7d069d3fd6605e5d5f609a8f733b59ec
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-27 16:32:12 +0530

    Update 17.2) Loops (break & continue).js

--- 17.2) Loops (break & continue).js
+++ 17.2) Loops (break & continue).js
@@ -1,1 +1,30 @@
 7) break statement:
+
+"for" example:
+for (let iteration = 1; iteration < 6; iteration++) {
+  console.log(iteration);
+}
+
+to stop it at 3 itself, will write, using break:
+  
+for (let iteration = 1; iteration < 6; iteration++) {
+  if (iteration === 4) {
+    break;
+  }
+  console.log(iteration);
+}
+
+Challenge:
+The for loop given below, displays all the elements of the array fruits. Use a break statement in the loop such that "Banana" and all the following elements are not displayed.
+
+Solution:
+const fruits = ["Apple", "Kiwi", "Mango", "Banana", "Orange", "Watermelon"];
+
+for (let index = 0; index < 6; index++) {
+  if (index === 3) {
+    break;
+  }
+  console.log(fruits[index]);
+}
+
+8) continue statement:
