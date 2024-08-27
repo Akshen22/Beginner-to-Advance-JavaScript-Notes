9bf0361841a333fdc200557192d5793bb126bb3c
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-27 08:32:49 +0530

    Update 16) Array Method 2.js

--- 16) Array Method 2.js
+++ 16) Array Method 2.js
@@ -108,9 +108,17 @@ vi) Challenge:
 In the code given below, the forEach method is used to create a new array containing all the elements of prices but with the "Rs. " prefix.
 But since the forEach method does not return an array with the transformed elements, replace it with the map method.
 
-Solution:
+Solution: 
+Method 1 (using: map):-
 const prices = [230, 420, 600];
 const pricesWithPrefix = prices.map(price => `Rs. ${price}`);
+console.log(pricesWithPrefix);
+
+Method 2 (using: forEach with console.log()):-
+const prices = [230, 420, 600];
+const pricesWithPrefix = prices.forEach(price => {
+  console.log(`Rs. ${price}`);
+});
 console.log(pricesWithPrefix);
 
 4) 
