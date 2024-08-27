69062b3994484037a83503d96d35c1f067492524
Akshen <54845610+Akshen22@users.noreply.github.com>
2024-08-27 23:15:31 +0530

    Update 18) Assessment 4.js

--- 18) Assessment 4.js
+++ 18) Assessment 4.js
@@ -73,3 +73,17 @@ const greetings = (message, callback) => {
 };
 
 5) Addition in Array:
+Create a function addThree, which accepts a number and increments it by 3 and returns the result.
+Create another function addThreeToArray which accepts an array of numbers as a parameter 
+and logs a new array where 3 is added to each element to the console.
+
+Solution:
+const addThree = (number) => number + 3;
+
+const addThreeToArray = (numberArray) => {
+  const newArray = [];
+  numberArray.forEach((element, index) => {
+    newArray[index] = addThree(element)
+  })
+  console.log(newArray);
+};
